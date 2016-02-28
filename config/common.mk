55b9c2c2f23bf78e2f596e2976da8efed432721b
fozzil4 <fozzil4>
2016-02-28 02:47:08 -0600

    Disable CyanogenMod Setup

--- config/common.mk
+++ config/common.mk
@@ -56,7 +56,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
     ro.com.android.wifi-watchlist=GoogleGuest \
-    ro.setupwizard.enterprise_mode=1 \
     ro.com.android.dateformat=MM-dd-yyyy \
     ro.com.android.dataroaming=false
 
@@ -153,7 +152,6 @@ PRODUCT_PACKAGES += \
 
 # Custom CM packages
 PRODUCT_PACKAGES += \
-    CyanogenSetupWizard \
     CMSettingsProvider \
     ExactCalculator
 
